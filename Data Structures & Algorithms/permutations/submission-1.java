cd5db7584439f220fc28ee2626f2621a252b4b84
Prathamesh Paraswar <70166818+prathameshp1703@users.noreply.github.com>
2026-06-12 19:22:41 +0530

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    private void solve(List<Integer> curr, List<List<Integer>> ans,
+     HashMap<Integer, Integer> freq, int[]nums){
+        if(curr.size() == nums.length){
+            List<Integer> temp = new ArrayList<>(curr);
+            ans.add(temp);
+            return;
+        }
+        for(int i=0; i<nums.length; i++){
+            if(!freq.containsKey(nums[i])){
+                freq.put(nums[i], 1);
+                curr.add(nums[i]);
+                solve(curr, ans, freq, nums);
+                curr.remove(curr.size()-1);
+                freq.remove(nums[i]);
+            }
+        }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        HashMap<Integer, Integer> freq = new HashMap<>();
+
+        List<Integer> curr = new ArrayList<>();
+        solve(curr, ans, freq, nums);
+
+        return ans;
+    }
+}
