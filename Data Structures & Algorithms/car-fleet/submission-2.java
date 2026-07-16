21af977488a6474b4f45a125ba7a138219262781
Prathamesh Paraswar <70166818+prathameshp1703@users.noreply.github.com>
2026-07-16 17:29:53 +0530

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.java
@@ -0,0 +1,62 @@
+class Solution {
+    class Pair{
+        private int first;
+        private int second;
+
+        Pair(int first, int second){
+            this.first = first;
+            this.second = second;
+        }
+    }
+
+    private double myCeil(int val, int div){
+        return (val*1.0)/div;
+    }
+
+    private boolean canBeMerged(Pair curr, Pair next, int target){
+        double steps1 = myCeil(target-curr.first, curr.second);
+        double steps2 = myCeil(target-next.first, next.second);
+
+        if(steps1 >= steps2) return true;
+        return false;
+    }
+
+    public int carFleet(int target, int[] position, int[] speed) {
+        PriorityQueue<Pair> fleet = new PriorityQueue<>((a,b)->{
+            if(a.first == b.first){
+                return Integer.compare(b.second, a.second);
+            }
+            return Integer.compare(b.first, a.first);
+        });
+
+        int fleets = 0;
+        int size = position.length;
+        for(int i=0; i<size; i++){
+            fleet.add(new Pair(position[i], speed[i]));
+        }
+        while(fleet.size()>0){
+            Pair curr = fleet.poll();
+            if(fleet.size()>0){
+                Pair next = fleet.poll();
+                
+                if(canBeMerged(curr, next, target)){
+                    // System.out.println(curr.first + " " + curr.second + " " +
+                //  next.first + " " + next.second);
+                    fleet.add(curr);
+                }
+                else{
+                    // System.out.println(curr.first + " " + curr.second + " " +
+                //  next.first + " " + next.second);
+                    fleets++;
+                    fleet.add(next);
+                }
+            }
+            else{
+                fleets++;
+                continue;
+            }
+        }
+
+        return fleets;
+    }
+}
